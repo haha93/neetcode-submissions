176ad39ba35c409a44a6108c61de70c2862c9202
haha93 <balu.sasi93@gmail.com>
2026-04-20 12:05:19 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+       let left = 0;
+       let right = 1;
+       let profit =0;
+       while(right < prices.length){
+        if(prices[right] > prices[left]){
+            profit = Math.max(profit,prices[right]-prices[left]);
+        }else{
+            left = right
+        }
+        right++;
+       }
+
+       return profit;
+       
+    }
+}
